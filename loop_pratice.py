7056ff1a3a31867d61b51d76eed0dcc2cbb60e45
paridapratik2007-bit <paridapratik2007@gmail.com>
2025-09-20 20:31:04 +0530

    Add files via upload

---
+++ loop_pratice.py
@@ -0,0 +1,35 @@
+'''n = int(input("no.: "))
+x = int(input("enter no.:"))
+
+for x in range (1,x+1):
+    print(n,"X",x,"=",n*x)''' # multiplicatin of table
+
+
+'''l = ["pratik","subham","sohan","chida","vineet","surya","smita"]
+for name in l:
+    if(name.startswith("s")):
+        print("Hello",name)''' # in list names which starts with "s"
+
+'''number = int(input())
+for i in range(2,number):
+    if(number%i == 0):
+        print("not a prime")
+        break
+else:
+     print("prime")''' # Prime number 
+
+
+'''n = int(input())
+s = 0
+i = 1
+while(i<=n):
+    s+=i
+    i = i+1
+
+print(s)''' # sum of natural numbers
+
+'''n = int(input())
+pro = 1
+for i in range(1,n+1):
+    pro = pro*i
+print(pro)''' # factorial number 
